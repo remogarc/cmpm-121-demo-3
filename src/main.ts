271a44ac99252080ae3a74e48cdfafd74d1d5f41
Reese Garcia <remogarc@ucsc.edu>
2023-10-31 10:56:20 -0700

    L14 Quiz

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,9 @@ import leaflet from "leaflet";
 import luck from "./luck";
 import "./leafletWorkaround";
 
-
 const MERRILL_CLASSROOM = leaflet.latLng({
     lat: 36.9995,
-    lng: - 122.0533
+    lng: -122.0533,
 });
 
 const GAMEPLAY_ZOOM_LEVEL = 19;
@@ -23,17 +22,30 @@ const map = leaflet.map(mapContainer, {
     minZoom: GAMEPLAY_ZOOM_LEVEL,
     maxZoom: GAMEPLAY_ZOOM_LEVEL,
     zoomControl: false,
-    scrollWheelZoom: false
+    scrollWheelZoom: false,
 });
 
-leaflet.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    maxZoom: 19,
-    attribution: "&copy; <a href=\"http://www.openstreetmap.org/copyright\">OpenStreetMap</a>"
-}).addTo(map);
+leaflet
+    .tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        maxZoom: 19,
+        attribution:
+            "&copy; <a href=\"http://www.openstreetmap.org/copyright\">OpenStreetMap</a>",
+    })
+    .addTo(map);
 
 const playerMarker = leaflet.marker(MERRILL_CLASSROOM);
 playerMarker.bindTooltip("That's you!");
 playerMarker.addTo(map);
+
+const sensorButton = document.querySelector("#sensor")!;
+sensorButton.addEventListener("click", () => {
+    navigator.geolocation.watchPosition((position) => {
+        playerMarker.setLatLng(
+            leaflet.latLng(position.coords.latitude, position.coords.longitude)
+        );
+        map.setView(playerMarker.getLatLng());
+    });
+});
 
 let points = 0;
 const statusPanel = document.querySelector<HTMLDivElement>("#statusPanel")!;
@@ -41,15 +53,17 @@ statusPanel.innerHTML = "No points yet...";
 
 function makePit(i: number, j: number) {
     const bounds = leaflet.latLngBounds([
-        [MERRILL_CLASSROOM.lat + i * TILE_DEGREES,
-        MERRILL_CLASSROOM.lng + j * TILE_DEGREES],
-        [MERRILL_CLASSROOM.lat + (i + 1) * TILE_DEGREES,
-        MERRILL_CLASSROOM.lng + (j + 1) * TILE_DEGREES],
+        [
+            MERRILL_CLASSROOM.lat + i * TILE_DEGREES,
+            MERRILL_CLASSROOM.lng + j * TILE_DEGREES,
+        ],
+        [
+            MERRILL_CLASSROOM.lat + (i + 1) * TILE_DEGREES,
+            MERRILL_CLASSROOM.lng + (j + 1) * TILE_DEGREES,
+        ],
     ]);
 
     const pit = leaflet.rectangle(bounds) as leaflet.Layer;
-
-
 
     pit.bindPopup(() => {
         let value = Math.floor(luck([i, j, "initialValue"].toString()) * 100);
@@ -60,7 +74,8 @@ function makePit(i: number, j: number) {
         const poke = container.querySelector<HTMLButtonElement>("#poke")!;
         poke.addEventListener("click", () => {
             value--;
-            container.querySelector<HTMLSpanElement>("#value")!.innerHTML = value.toString();
+            container.querySelector<HTMLSpanElement>("#value")!.innerHTML =
+                value.toString();
             points++;
             statusPanel.innerHTML = `${points} points accumulated`;
         });
@@ -70,7 +85,7 @@ function makePit(i: number, j: number) {
 }
 
 for (let i = -NEIGHBORHOOD_SIZE; i < NEIGHBORHOOD_SIZE; i++) {
-    for (let j = - NEIGHBORHOOD_SIZE; j < NEIGHBORHOOD_SIZE; j++) {
+    for (let j = -NEIGHBORHOOD_SIZE; j < NEIGHBORHOOD_SIZE; j++) {
         if (luck([i, j].toString()) < PIT_SPAWN_PROBABILITY) {
             makePit(i, j);
         }
